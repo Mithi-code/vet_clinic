f5117974ac30f43c41e022ee0cfc682eb266dc9c
mithlesh kumar <mithlesh.9m@gmail.com>
2022-02-14 19:13:24 +0530

    added animals names

--- data.sql
+++ data.sql
@@ -1,5 +1,7 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (name) VALUES ('Agumon');
+INSERT INTO animals (name) VALUES ('Gabumon');
+INSERT INTO animals (name) VALUES ('Pikachu');
+INSERT INTO animals (name) VALUES ('Devimon');
+
