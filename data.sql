9a3c1bea3dfa943a22df8e30cc2795e9216a84bc
mithlesh kumar <mithlesh.9m@gmail.com>
2022-02-14 19:47:04 +0530

    added tables and queries

--- data.sql
+++ data.sql
@@ -1,7 +1,64 @@
 /* Populate database with sample data. */
+INSERT INTO animals(
+    id,
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+) VALUES (
+        1, 
+        'Agumon',
+        '2020-02-03',
+        0,
+        TRUE,
+        10.23
+);
 
-INSERT INTO animals (name) VALUES ('Agumon');
-INSERT INTO animals (name) VALUES ('Gabumon');
-INSERT INTO animals (name) VALUES ('Pikachu');
-INSERT INTO animals (name) VALUES ('Devimon');
+INSERT INTO animals(
+    id,
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+) VALUES (
+    2, 
+    'Gabumon',
+    '2018-11-15',
+    2,
+    TRUE,
+    8
+);
 
+INSERT INTO animals(
+    id,
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+) VALUES(
+    3,
+    'Pikachu',
+    '2021-01-7',
+    1,
+    FALSE,
+    15.08
+);
+
+INSERT INTO animals(
+    id,
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+) VALUES(
+    4,
+    'Devimon',
+    '2017-05-12',
+    5,
+    TRUE,
+    11
+);
--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT PRIMARY KEY NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    date_of_birth Date NOT NULL,
+    id INT PRIMARY KEY NOT NULL, 
+    name VARCHAR(100) NOT NULL, 
+    date_of_birth DATE NOT NULL, 
     escape_attempts INT NOT NULL,
     neutered BOOLEAN NOT NULL,
-    weight_kg: DECIMAL NOT NULL
-);
+    weight_kg DECIMAL NOT NULL
+  );
