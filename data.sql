d2925b9f40ec5a655a273d0b953a0309b3cb682e
mithlesh kumar <mithlesh.9m@gmail.com>
2022-02-15 20:31:10 +0530

    update table data

--- data.sql
+++ data.sql
@@ -1,4 +1,5 @@
 /* Populate database with sample data. */
+
 INSERT INTO animals(
     id,
     name,
@@ -62,3 +63,152 @@ INSERT INTO animals(
     TRUE,
     11
 );
+
+/*Inserting new data to table*/
+
+INSERT INTO animals(
+    id,
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+) VALUES (
+        5, 
+        'Charmander',
+        '2020-02-08',
+        0,
+        FALSE,
+        11
+);
+
+INSERT INTO animals(
+    id,
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+) VALUES (
+    6, 
+    'Plantmon',
+    '2022-11-15',
+    2,
+    TRUE,
+    5.7
+);
+
+INSERT INTO animals(
+    id,
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+) VALUES(
+    7,
+    'Squirtle',
+    '1993-04-02',
+    3,
+    FALSE,
+    12.13
+);
+
+INSERT INTO animals(
+    id,
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+) VALUES(
+    8,
+    'Angemon',
+    '2005-06-12',
+    1,
+    TRUE,
+    45
+);
+
+INSERT INTO animals(
+    id,
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+) VALUES(
+    9,
+    'Boarmon',
+    '2005-06-07',
+    7,
+    TRUE,
+    20.4
+);
+
+INSERT INTO animals(
+    id,
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+) VALUES(
+    10,
+    'Blossom',
+    '1998-10-13',
+    3,
+    TRUE,
+    17
+);
+
+
+/* Start a transaction and update the animals 
+   table by setting the species column to unspecified.
+*/
+BEGIN TRANSACTION; -- start transaction
+
+UPDATE animals SET species = 'unspecified';
+
+--verify the changes were made
+SELECT * FROM animals;
+
+--rollback changes made to species
+ROLLBACK TRANSACTION;
+
+/* Start a transaction and update the animals 
+   table by setting species column
+*/
+BEGIN TRANSACTION;
+
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+
+COMMIT TRANSACTION;
+
+SELECT name, species FROM animals;
+
+/* Start a transaction and delete 
+   all records in the animals table
+*/
+BEGIN TRANSACTION;
+
+DELETE FROM animals;
+
+ROLLBACK TRANSACTION;
+
+-- start and run multiple transactions on the animals table
+BEGIN TRANSACTION;
+
+DELETE FROM animals WHERE date_of_birth > 'Jan 1, 2022';
+
+SAVE TRANSACTION first_deleted_transaction;
+
+UPDATE animals SET weight_kg = weight_kg * -1;
+
+ROLLBACK TRANSACTION first_deleted_transaction;
+
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+COMMIT TRANSACTION;
