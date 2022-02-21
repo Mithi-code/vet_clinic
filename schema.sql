9cd97650ce2d91122486da02a0c55d52915022fd
mithlesh kumar <mithlesh.9m@gmail.com>
2022-02-21 19:26:04 +0530

    added perfomance

--- schema.sql
+++ schema.sql
@@ -68,3 +68,6 @@ CREATE TABLE visits (
     FOREIGN KEY (animal_id) REFERENCES animals (id),
     FOREIGN KEY (vet_id) REFERENCES vets (id) 
 );
+
+-- Add an email column to your owners table
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
