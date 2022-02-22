6f3d35808dc0abbd0cdebdee188a21fe6ee603c7
mongare <mongaresams@gmail.com>
2022-02-22 11:46:29 +0300

    Update readme.md and Add indexing

--- schema.sql
+++ schema.sql
@@ -71,3 +71,8 @@ CREATE TABLE visits (
 
 -- Add an email column to your owners table
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+-- optimize queries performance 
+CREATE INDEX visits_animals_id ON visits(animal_id);
+CREATE INDEX visits_vets_id ON visits(vets_id);
+CREATE INDEX visits_owners_id ON owners(email); 
