1af04c39ce907b573341283429f69e7a8d02a17f
mongare <mongaresams@gmail.com>
2022-02-23 11:16:33 +0300

    Add medical histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -5,3 +5,13 @@ CREATE TABLE patients (
   PRIMARY KEY (id)
 );
 
+CREATE TABLE medical_histories (
+id INT GENERATED ALWAYS AS IDENTITY,
+admitted_at TIMESTAMP,
+patient_id INT,
+status VARCHAR(255),
+ADD CONSTRAINT fk_patients
+FOREIGN KEY (patient_id)
+REFERENCES patients(id),
+PRIMARY KEY (id)
+); 
