41a080a3018baaaa62f61a48a9f0a8d00d432353
mithlesh kumar <mithlesh.9m@gmail.com>
2022-02-23 13:49:52 +0530

    added invoice item table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -34,3 +34,19 @@ type VARCHAR(255),
 name VARCHAR(255),
 PRIMARY KEY (id)
 ); 
+
+CREATE TABLE invoice_items (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  unit_price DECIMAL,
+  quantity INT,
+  total_price DECIMAL,
+  invoice_id INT,
+  treatment_id INT,
+  ADD CONSTRAINT fk_invoices
+  FOREIGN KEY (invoice_id)
+  REFERENCES invoices(id),
+  ADD CONSTRAINT fk_treatments
+  FOREIGN KEY (treatment_id)
+  REFERENCES treatments(id),
+  PRIMARY KEY (id)
+);
